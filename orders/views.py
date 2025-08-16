12e21a4c50d8611276bd21e0b84364581315eea6
HashviM <mhashvitha1990@gmail.com>
2025-08-16 16:26:53 +0530

    feat(ui): bootstrap base, navbar, footer, and page scaffolds

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def track_order(request):
+    return render(request, "orders/track_order.html")
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def product_list(request):
+    return render(request, "shop/product_list.html")
+
+def product_detail(request, pk):
+    return render(request, "shop/product_detail.html", {"pk": pk})
